e82d77089181a59e554e77182d53e781d2461a44
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-12 10:38:25 +0200

    else if

--- public/js/enums.js
+++ public/js/enums.js
@@ -1,4 +1,16 @@
 "use strict";
 // 1. sukurti enmu kuriame galimos reiksme yra baltijos salys
-// sukutri kintamaji kuriame bus stringas ir jo reiksme
-// as gyvenu [baltijos salis is enum]
+const salisString = `as gyvenu ${"Lietuva" /* BaltStates.lt */}`;
+// 2.1 sukurti skaiciuotuva kuris priima 2 skaicius ir veiksma ir grazina rezultata
+function calculate(num1, num2, action) {
+    // apskaiciavimas
+    if (action === 0 /* CalcActions.plus */) {
+        const rez = num1 + num2;
+        console.log('rez ===', rez);
+    }
+    else if (action === 1 /* CalcActions.minus */) {
+        const rez = num1 - num2;
+        console.log('rez ===', rez);
+    }
+}
+calculate(5, 10, 1 /* CalcActions.minus */);
--- src/enums.ts
+++ src/enums.ts
@@ -1,5 +1,35 @@
 // 1. sukurti enmu kuriame galimos reiksme yra baltijos salys
 
+const enum BaltStates {
+  lt = 'Lietuva',
+  lv = 'Latvija',
+  est = 'Estija',
+}
+
+const salisString: string = `as gyvenu ${BaltStates.lt}`;
+// console.log('salisString ===', salisString);
 // sukutri kintamaji kuriame bus stringas ir jo reiksme
 
 // as gyvenu [baltijos salis is enum]
+
+// 2. sukurti enum CalcActions kuris gali but plus, minus, divide, multiply
+
+const enum CalcActions {
+  plus,
+  minus,
+  divide,
+  multiply,
+}
+// 2.1 sukurti skaiciuotuva kuris priima 2 skaicius ir veiksma ir grazina rezultata
+
+function calculate(num1: number, num2: number, action: CalcActions) {
+  // apskaiciavimas
+  if (action === CalcActions.plus) {
+    const rez = num1 + num2;
+    console.log('rez ===', rez);
+  } else if (action === CalcActions.minus) {
+    const rez = num1 - num2;
+    console.log('rez ===', rez);
+  }
+}
+calculate(5, 10, CalcActions.minus);
