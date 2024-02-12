ebaab2fd20cceb3dacf2b58be6e6fe27a0fb5ac3
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-12 09:43:43 +0200

    breed

--- public/js/keyOf.js
+++ public/js/keyOf.js
@@ -1,9 +1,10 @@
 "use strict";
 const dog1 = {
     name: 'Blupers',
-    breed: 'chichuachua',
+    breed: 'poodel',
     longHair: true,
 };
+const breed1 = 'doberman';
 // isAdult: true,
 const cat1 = {
     name: 'Mitcy',
--- src/keyOf.ts
+++ src/keyOf.ts
@@ -1,14 +1,20 @@
 type Dog = {
-  breed: 'doberman' | 'chichuachua';
+  breed: 'doberman' | 'chichuachua' | 'poodel';
   longHair: boolean;
   name: string;
 };
 
 const dog1: Dog = {
   name: 'Blupers',
-  breed: 'chichuachua',
+  breed: 'poodel',
   longHair: true,
 };
+
+// sukurti tipa is visu galimu breed reiksmiu
+type DogBreeds = Dog['breed'];
+
+const breed1: Dog['breed'] = 'doberman';
+
 // cat tipas gali tureti daug savybiu,
 // kievienas key bus string,
 // kiekviena value bus string arba number
