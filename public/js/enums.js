1a6478425b7fc960ffc984e555a74504491f84fd
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-12 10:09:57 +0200

    enum

---
+++ public/js/enums.js
@@ -0,0 +1,4 @@
+"use strict";
+// 1. sukurti enmu kuriame galimos reiksme yra baltijos salys
+// sukutri kintamaji kuriame bus stringas ir jo reiksme
+// as gyvenu [baltijos salis is enum]
---
+++ src/enums.ts
@@ -0,0 +1,5 @@
+// 1. sukurti enmu kuriame galimos reiksme yra baltijos salys
+
+// sukutri kintamaji kuriame bus stringas ir jo reiksme
+
+// as gyvenu [baltijos salis is enum]
