6eda60f98fd65f6b33d2e81da829a2347f011b69
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-12 09:59:33 +0200

    utility

--- public/js/keyOf.js
+++ public/js/keyOf.js
@@ -10,3 +10,8 @@ const cat1 = {
     name: 'Mitcy',
     age: 5,
 };
+const addr1 = {
+    street: 'Vytauto',
+    houseNr: 15,
+    town: 'Kaunas',
+};
--- src/keyOf.ts
+++ src/keyOf.ts
@@ -19,7 +19,7 @@ const breed1: Dog['breed'] = 'doberman';
 // kievienas key bus string,
 // kiekviena value bus string arba number
 type Cat = {
-  [key: string]: string | number;
+  [s: string]: string | number;
 };
 
 // isAdult: true,
@@ -27,3 +27,64 @@ const cat1: Cat = {
   name: 'Mitcy',
   age: 5,
 };
+
+type Address = {
+  street: string;
+  houseNr: number;
+  town: string;
+  postCode?: string;
+};
+
+// sukurti nauja tipa kuris tures visus key kaip ir Address
+type AddressSimilar = {
+  [propertyName in keyof Address]: string;
+};
+
+// sukurti pilna kopija Address
+type AddressCopy = {
+  [propertyName in keyof Address]: Address[propertyName];
+};
+
+// sukuti tipa addressas be post codo
+type AddresNoPost = Omit<Address, 'postCode'>;
+
+// tipas kur visos savybes nebutinos
+type AddressOptional = Partial<Address>;
+
+// tpas kur visos savybes privalomos
+type AddressWithPostCode = Required<Address>;
+
+const addr1: Address = {
+  street: 'Vytauto',
+  houseNr: 15,
+  town: 'Kaunas',
+};
+const addr2: AddressWithPostCode = {
+  street: 'Vytauto',
+  houseNr: 15,
+  town: 'Kaunas',
+};
+
+interface UserFull {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: number;
+      lng: number;
+    };
+  };
+  phone: string;
+  website: string;
+  company?: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
